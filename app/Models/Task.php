23d7890676e6bf465300fdbd7851378164f586e2
Vodennikov D. A. <vodennikov@yandex.ru>
2026-02-08 16:42:46 +0500

    Add status constants to Task model   Add STATUS_UNCOMPLETE and STATUS_COMPLETE constants to Task model   Task model minor fixes

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Task extends Model
 {
-    //
+    use HasFactory;
+    
+    const STATUS_UNCOMPLETE = 0;
+    const STATUS_COMPLETE   = 1;
+    
+    protected $fillable = [
+		'title',
+		'description',
+		'status'
+    ];
 }
