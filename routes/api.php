aa5c1ee9dd9d29e353cbd6a063becd1243820c52
Vodennikov D. A. <vodennikov@yandex.ru>
2026-02-12 19:26:40 +0500

    Add routes for AuthController to routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,12 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::prefix('v1')->middleware(['throttle:api'])->group(function() {
+	Route::post('register', [\App\Http\Controllers\Api\V1\AuthController::class, 'register']);
+	Route::post('login', [\App\Http\Controllers\Api\V1\AuthController::class, 'login']);
+});
+
 Route::prefix('v1')->middleware(['throttle:api', 'auth:sanctum'])->group(function() {
-	Route::apiResource(
-		'tasks',
-	    \App\Http\Controllers\Api\V1\TaskController::class);
+	Route::apiResource('tasks', \App\Http\Controllers\Api\V1\TaskController::class);
+	Route::get('logout', [\App\Http\Controllers\Api\V1\AuthController::class, 'logout']);
 });
